fd3379bde2643b5be9f31e26a0780c12eed80760
kopibean101223 <151587474+kopibean101223@users.noreply.github.com>
2024-06-05 00:43:12 +0800

    Add files via upload

--- BORROWBOOK_PAGE.java
+++ BORROWBOOK_PAGE.java
@@ -24,10 +24,10 @@ public static void main (String[] args) {
 
 JFrame f=new JFrame("TMC'S LIBRARY MANAGEMENT SYSTEM");
 f. getContentPane(). setLayout(null);
-
+Font fontop = new Font("BAHNSCHRIFT", Font.BOLD, 20);
 
 f.add(comboBox);
-comboBox.setBounds(350, 585,300, 30);
+comboBox.setBounds(350, 565,300, 30);
 //HEADER{
 JLabel label1 = new JLabel(" WELCOME TO TMC'S LIBRARY MANAGEMENT SYSTEM");
 JLabel label2 = new JLabel("");
@@ -37,6 +37,7 @@ label2.setBounds(0, 0,1440, 169);
 label1.setBounds(100, 0,1440, 169);
 
 Font fonttitle = new Font("BAHNSCHRIFT", Font.BOLD, 48);
+Font fonttitle2 = new Font("BAHNSCHRIFT", Font.BOLD, 14);
 label1.setFont(fonttitle);
 label1.setOpaque(true);
 Color customColor = new Color(255, 204, 51);
@@ -50,7 +51,7 @@ label2.setOpaque(true);
 //FORM
 JLabel label3 = new JLabel("BOOK BORROWING FORM");
 f.add(label3);
-label3.setBounds(500, 200,800, 169);
+label3.setBounds(500, 180,800, 169);
 label3.setFont(fonttitle);
 label3.setBackground(Color.BLACK);
 //FORM
@@ -58,139 +59,145 @@ label3.setBackground(Color.BLACK);
 //LINE
 JLabel label4 = new JLabel("");
 f.add(label4);
-label4.setBounds(350, 320,950, 1);
+label4.setBounds(350, 300,950, 1);
 label4.setFont(fonttitle);
 label4.setOpaque(true);
 label4.setBackground(Color.BLACK);
 //LINE
 
+//LINE
+JLabel label5 = new JLabel("");
+f.add(label5);
+label5.setBounds(350, 495,950, 1);
+label5.setFont(fonttitle);
+label5.setOpaque(true);
+label5.setBackground(Color.BLACK);
+//LINE
 JTextField idchecker;
 
-JLabel idchkerlbl =new JLabel ("ID");
+JLabel idchkerlbl =new JLabel ("ID:");
+idchkerlbl.setFont(fonttitle2);
 idchecker=new JTextField(15);
-idchkerlbl.setBounds(350, 290,300, 30);
-idchecker.setBounds(350, 320,950, 30);
+idchkerlbl.setBounds(350, 330,300, 30);
+idchecker.setBounds(350, 360,630, 30);
 f.add(idchkerlbl);
 f.add(idchecker);
+idchecker.setFont(fontop);
 
 
-JLabel brdtlbl = new JLabel("Date Borrowed: (TODAY)");
+JLabel brdtlbl = new JLabel("DATE BORROWED: (TODAY)");
 f.add(brdtlbl);
-brdtlbl.setBounds(350, 660,300, 30);
-JLabel dtdulbl = new JLabel("Due Date: ");
+brdtlbl.setFont(fonttitle2);
+brdtlbl.setBounds(350, 620,300, 30);
+JLabel dtdulbl = new JLabel("DUE DATE: ");
 f.add(dtdulbl);
-dtdulbl.setBounds(675, 660,300, 30);
+dtdulbl.setFont(fonttitle2);
+dtdulbl.setBounds(675, 620,300, 30);
 	
 	Color customColor3 = new Color(224, 228, 230);
 
-	
+	JLabel lblfname=new JLabel ("FIRST NAME: ");
+	lblfname.setBounds(350, 400,300, 30);
+	lblfname.setFont(fonttitle2);
+    f.add(lblfname);
+    JLabel lblmname=new JLabel ("LAST NAME: ");
+    lblmname.setBounds(675, 400,300, 30);
+    lblmname.setFont(fonttitle2);
+    f.add(lblmname);
+    JLabel lblinitial=new JLabel ("MIDDLE INITIAL: ");
+    lblinitial.setBounds(1000, 400,300, 30);
+    lblinitial.setFont(fonttitle2);
+    f.add(lblinitial);
+    JLabel lbltype=new JLabel ("TYPE: ");
+    lbltype.setBounds(1155, 400,300, 30);
+    lbltype.setFont(fonttitle2);
+    f.add(lbltype);
+    
+
 	JTextField ttl =new JTextField ();
 	f.add(ttl);
- 	ttl.setBounds(675, 585,300, 30);
+ 	ttl.setBounds(675, 565,300, 30);
  	ttl.setOpaque(true);
  	ttl.setEditable(false);
  	ttl.setBackground(customColor3);
  	ttl.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
  	ttl.setForeground(Color.BLACK);
+ 	ttl.setFont(fontop);
  	
  	JTextField stts =new JTextField ();
  	f.add(stts);
- 	stts.setBounds(1000, 585,300, 30);
+ 	stts.setBounds(1000, 565,300, 30);
  	stts.setOpaque(true);
  	stts.setEditable(false);
  	stts.setBackground(customColor3);
  	stts.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
  	stts.setForeground(Color.BLACK);
-	
+ 	stts.setFont(fontop);
+ 	
  	JTextField brdt =new JTextField ();
 	f.add(brdt);
-	brdt.setBounds(350, 690,300, 30);
+	brdt.setBounds(350, 650,300, 30);
 	brdt.setOpaque(true);
 	brdt.setEditable(false);
 	brdt.setBackground(customColor3);
 	brdt.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
 	brdt.setForeground(Color.BLACK);
+	brdt.setFont(fontop);
 	
 	JTextField dtdu =new JTextField ();
  	f.add(dtdu);
- 	dtdu.setBounds(675, 690,300, 30);
+ 	dtdu.setBounds(675, 650,300, 30);
  	dtdu.setOpaque(true);
  	dtdu.setEditable(false);
  	dtdu.setBackground(customColor3);
  	dtdu.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
  	dtdu.setForeground(Color.BLACK);
+ 	dtdu.setFont(fontop);
  	
  	JTextField fname = new JTextField ();
  	f.add(fname);
- 	fname.setBounds(350, 400,300, 30);
+ 	fname.setBounds(350, 430,300, 30);
  	fname.setOpaque(true);
  	fname.setEditable(false);
  	fname.setBackground(customColor3);
  	fname.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
  	fname.setForeground(Color.BLACK);
+ 	fname.setFont(fontop);
  	
  	JTextField lname =new JTextField ();
  	f.add(lname);
- 	lname.setBounds(675, 400,300, 30);
+ 	lname.setBounds(675, 430,300, 30);
  	lname.setOpaque(true);
  	lname.setEditable(false);
  	lname.setBackground(customColor3);
  	lname.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
  	lname.setForeground(Color.BLACK);
- 	
+ 	lname.setFont(fontop);
  	
  	JTextField mname =new JTextField ();
  	
  	f.add(mname);
- 	mname.setBounds(1000, 400,145, 30);
+ 	mname.setBounds(1000, 430,145, 30);
  	mname.setOpaque(true);
  	mname.setEditable(false);
  	mname.setBackground(customColor3);
  	mname.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
  	mname.setForeground(Color.BLACK);
+ 	mname.setFont(fontop);
  	
  	JTextField type =new JTextField ();
  	f.add(type);
  	
- 	type.setBounds(1155, 400,145, 30);
+ 	type.setBounds(1155, 430,145, 30);
  	type.setOpaque(true);
  	type.setEditable(false);
  	type.setBackground(customColor3);
  	type.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
  	type.setForeground(Color.BLACK);
+ 	type.setFont(fontop);
  	
  	
 
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
-JLabel lbldateb=new JLabel ("Date Borrowed:");
-    
-    lbldateb.setBounds(350, 690,300, 30);
-    f.add(lbldateb);
-
-    
-   
-    // Get today's date
     LocalDate today = LocalDate.now();
 
     // Format the date as desired
@@ -201,35 +208,21 @@ JLabel lbldateb=new JLabel ("Date Borrowed:");
   
 
 
-    
-    //BORROWER TYPE WITH TEXTBOX
-    
-    //BORROWER TYPE WITH TEXTBOX
 
-
-
-    JLabel lbldued=new JLabel ("DUE DATE:");
-    lbldued.setBounds(830, 690,470, 30);
-    f.add(lbldued);
     // Set today's date in the text field
     LocalDate outputDate = today.plusDays(3);
     String formattedDate2 = outputDate.format(formatter);
-   
-
-
-
- 	
- 	
- 	
- 	
- 	
- 	
+    
+    
+    
+    
+    		
  	
  	
  	
  	
 JButton chckbtn=new JButton("Check For Record");
-chckbtn.setBounds (602, 500,450, 40);
+chckbtn.setBounds (1000, 360,300, 30);
 chckbtn.addActionListener(new ActionListener(){
 public void actionPerformed (ActionEvent e){
 	String values = idchecker.getText().trim();
@@ -237,6 +230,10 @@ public void actionPerformed (ActionEvent e){
         JOptionPane.showMessageDialog(f, "Please enter a value in the text field.", "Error", JOptionPane.ERROR_MESSAGE);
         return;
     }
+    fname.setText("");
+    lname.setText("");
+    mname.setText("");
+    type.setText("");
     
     
    
@@ -250,10 +247,10 @@ public void actionPerformed (ActionEvent e){
 									 	    // Connect to the database
 									 	    Connection conn = DriverManager.getConnection(url, "root", "");
 									 	    Statement statement = conn.createStatement();
-									 	    
-									 	 //COMBO BOX AND DISPLAY 	    
 									 	   comboBox.removeAllItems();
-									 	  comboBox.addItem(null);
+										 	  comboBox.addItem(null);
+									 	 //COMBO BOX AND DISPLAY 	    
+									 	   	
 									 	 
 									 	 
 									 	   ResultSet resultSetss = statement.executeQuery("SELECT LASTNAME, FIRSTNAME, INITIAL,TYPE FROM BORROWER_TABLE WHERE ID = "+"'"+idchecker.getText()+"';");
@@ -265,10 +262,12 @@ public void actionPerformed (ActionEvent e){
 									 		mname.setText(resultSetss.getString("INITIAL"));
 									 		type.setText(resultSetss.getString("TYPE"));
 									 		
+									 		
  	 //COMBO BOX AND DISPLAY				
 									 	  } else {
 						                        JOptionPane.showMessageDialog(f, "The ID you entered does not exist!", "ERROR", JOptionPane.INFORMATION_MESSAGE);
-										    }
+										  return;  
+									 	  }
  	    
  	    resultSetss.close();
  	    statement.close();
@@ -297,7 +296,7 @@ try {// Load the MySQL driver
     
     
 
-    ResultSet resultSet = statement.executeQuery("SELECT TITLE FROM BOOK_DETAILS WHERE STATUS = 'AVAILABLE';");
+    ResultSet resultSet = statement.executeQuery("SELECT TITLE FROM BOOK_DETAILS WHERE STATUS != 'BORROWED' ORDER BY CATEGORY;");
 
     while (resultSet.next()) {
     	String value = resultSet.getString(1);
@@ -310,7 +309,7 @@ try {// Load the MySQL driver
     }
     
     
-    
+    comboBox.setFont(fontop);
     
     comboBox.addItemListener(new ItemListener() {
         @Override
@@ -407,27 +406,31 @@ f.add(chckbtn);
          //BORROWER TYPE WITH TEXTBOX
 
 
-         JLabel lbl6=new JLabel ("Borrowed Books: ");
-         lbl6.setBounds(350, 550,300, 30);
+         JLabel lbl6=new JLabel ("BOOK TO BE BORROWED: ");
+         lbl6.setBounds(350, 530,300, 30);
+         lbl6.setFont(fonttitle2);
          f.add(lbl6);
-         f.add(lbl6);
+         
+         JLabel lblbooktype=new JLabel ("BOOK TYPE: ");
+         lblbooktype.setBounds(675, 530,300, 30);
+         lblbooktype.setFont(fonttitle2);
+         f.add(lblbooktype);
+         
+         JLabel lblbookstatus=new JLabel ("BOOK STATUS: ");
+         lblbookstatus.setBounds(1000, 530,300, 30);
+         lblbookstatus.setFont(fonttitle2);
+         f.add(lblbookstatus);
          
 
 
          //BORROWER TYPE WITH TEXTBOX
          
 
-         
-         
-         
-         
-         
-         
      
          
 // HOME BUTTON
 JButton subton=new JButton("BORROW");
-subton. setBounds (350, 800,950, 50);
+subton. setBounds (1000, 620,300, 60);
 subton.addActionListener(new ActionListener(){
 public void actionPerformed (ActionEvent e){
 	String values = idchecker.getText().trim();
@@ -463,9 +466,10 @@ public void actionPerformed (ActionEvent e){
         Connection conn = DriverManager.getConnection(url, "root", "");
         Statement statement = conn.createStatement();
 
-        // COMBO BOX AND DISPLAY
-        comboBox.removeAllItems();
-        comboBox.addItem(null);
+       
+	    	    
+	    	
+	    	
 
         // Fetch the type of the borrower
         ResultSet resultSet1 = statement.executeQuery("SELECT TYPE FROM BORROWER_TABLE WHERE ID = '" + idchecker.getText() + "';");
@@ -523,7 +527,7 @@ public void actionPerformed (ActionEvent e){
    			   			String query3 = ("UPDATE BOOK_DETAILS SET BORROWER_ID = "+"'"+idchecker.getText()+"'"+" WHERE TITLE = "+"'"+ nmr  + "';");				
 			   					String query4 = ("UPDATE BOOK_DETAILS SET BORROWED_DATE = "+"'"+brdt.getText()+"'"+" WHERE TITLE = "+"'"+ nmr  + "';");
 						   				String query5 = ("UPDATE BOOK_DETAILS SET DATE_DUE = "+"'"+dtdu.getText()+"'"+" WHERE TITLE = "+"'"+ nmr  + "';");
-						   					String query6 = ("UPDATE BOOK_DETAILS SET PAYMENT = 'NOT YET PAID' WHERE TITLE = "+"'"+ nmr  + "';");
+
    				
 
    					
@@ -532,7 +536,7 @@ public void actionPerformed (ActionEvent e){
 	   				statement.executeUpdate(query3);
 	   				statement.executeUpdate(query4);
 	   				statement.executeUpdate(query5);
-	   				statement.executeUpdate(query6);
+
 	   				statement.close();
 	   	JOptionPane.showMessageDialog(f, "You have successfully borrowed " +nmr+"!", " THANK YOU FOR BORROWING!!!", JOptionPane.INFORMATION_MESSAGE);
         System.out.println("Inserted successfully!");
@@ -608,13 +612,13 @@ chckbtn.setForeground(Color.WHITE);
 
 JLabel bg = new JLabel("");
 f.add(bg);
-bg.setBounds(300, 220,1050, 700);
+bg.setBounds(300, 200,1050, 540);
 
 bg.setBackground(Color.WHITE);
 bg.setOpaque(true);
 
 // CUSTOMIZE FRAME}
-f.setSize(1440,1024);
+f.setSize(1440,800);
 f.setVisible(true);
 f.setResizable(false);
 // CUSTOMIZE FRAME}
